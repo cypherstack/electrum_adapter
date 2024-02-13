c1d666ea9b553e5e55bdf5a4ac35d1fbebe55c33
julian <julian@cypherstack.com>
2024-02-13 09:45:15 +0700

    Insert general electrum client

--- lib/electrum_adapter.dart
+++ lib/electrum_adapter.dart
@@ -5,10 +5,11 @@ library electrum_adapter;
 
 import 'dart:async';
 
-import 'connect.dart' as conn;
 import 'client/subscribing_client.dart';
+import 'connect.dart' as conn;
 import 'methods/server/version.dart';
 
+export 'connect.dart';
 export 'methods/asset/addresses.dart';
 export 'methods/asset/assets.dart';
 export 'methods/asset/meta.dart';
@@ -16,20 +17,17 @@ export 'methods/scripthash/balance.dart';
 export 'methods/scripthash/history.dart';
 export 'methods/scripthash/unspent.dart';
 export 'methods/server/features.dart';
-export 'methods/server/stats.dart';
 export 'methods/server/ping.dart';
+export 'methods/server/stats.dart';
 export 'methods/server/version.dart';
 export 'methods/transaction/broadcast.dart';
-export 'methods/transaction/memo.dart';
-export 'methods/transaction/get.dart';
 export 'methods/transaction/fee.dart';
-
-export 'subscriptions/subscribe_headers.dart';
+export 'methods/transaction/get.dart';
+export 'methods/transaction/memo.dart';
 export 'subscriptions/subscribe_asset.dart';
+export 'subscriptions/subscribe_headers.dart';
 export 'subscriptions/subscribe_scripthash.dart';
 export 'subscriptions/unsubscribe_scripthash.dart';
-
-export 'connect.dart';
 
 class Header {
   String hex;
@@ -39,13 +37,45 @@ class Header {
 
 /// Methods on RavenElectrumClient are defined in the `methods` directory.
 /// See https://electrumx-ravencoin.readthedocs.io/en/latest/protocol-methods.html
-class RavenElectrumClient extends SubscribingClient {
-  RavenElectrumClient(channel) : super(channel);
+class ElectrumClient extends SubscribingClient {
+  ElectrumClient(channel, String host, int port)
+      : this.host = host,
+        this.port = port,
+        super(channel);
+  late final String host;
+  late final int port;
+
+  static Future<ElectrumClient> connect({
+    required String host,
+    required int port,
+    Duration connectionTimeout = conn.connectionTimeout,
+    Duration aliveTimerDuration = conn.aliveTimerDuration,
+    bool acceptUnverified = true,
+  }) async {
+    final client = ElectrumClient(
+      await conn.connect(
+        host,
+        port: port,
+        connectionTimeout: connectionTimeout,
+        aliveTimerDuration: aliveTimerDuration,
+        acceptUnverified: acceptUnverified,
+      ),
+      host,
+      port,
+    );
+
+    return client;
+  }
+}
+
+/// Methods on RavenElectrumClient are defined in the `methods` directory.
+/// See https://electrumx-ravencoin.readthedocs.io/en/latest/protocol-methods.html
+class RavenElectrumClient extends ElectrumClient {
+  RavenElectrumClient(channel, {String host = '', int port = 50002})
+      : super(channel, host, port);
   String clientName = 'MTWallet';
   String clientVersion = '1.0';
-  String host = '';
   String protocolVersion = '1.10';
-  int port = 50002;
 
   static Future<RavenElectrumClient> connect(
     String host, {
@@ -57,17 +87,19 @@ class RavenElectrumClient extends SubscribingClient {
     String clientVersion = '1.0',
     String protocolVersion = '1.10',
   }) async {
-    var client = RavenElectrumClient(await conn.connect(
-      host,
+    var client = RavenElectrumClient(
+      await conn.connect(
+        host,
+        port: port,
+        connectionTimeout: connectionTimeout,
+        aliveTimerDuration: aliveTimerDuration,
+        acceptUnverified: acceptUnverified,
+      ),
+      host: host,
       port: port,
-      connectionTimeout: connectionTimeout,
-      aliveTimerDuration: aliveTimerDuration,
-      acceptUnverified: acceptUnverified,
-    ));
+    );
     client.clientName = clientName;
-    client.host = host;
     client.protocolVersion = protocolVersion;
-    client.port = port;
     await client.serverVersion(
         clientName: '$clientName/$clientVersion',
         protocolVersion: protocolVersion);
